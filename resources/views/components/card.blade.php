5ba0065f081062d3fa96bfcfea37cb71f5f463f0
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-03 17:12:05 -0500

    Add Card-component

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <div class="card-header">
+        <div class="card-title">Titulo</div>
+        <div class="card-tools">
+
+        </div>
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer">
+
+        </div>
+    </div>
+</div>
