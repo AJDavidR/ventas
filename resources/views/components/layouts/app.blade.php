20ca4dfdc0028d0c73250f66fa99da1e47bfc99b
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-03 15:39:47 -0500

    Add: footer to partial

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -66,13 +66,7 @@
 
 
         <!-- Main Footer -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2023 <a href="#">EMPRESA.io</a>.</strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 1.0.0
-            </div>
-        </footer>
+        @include('components.layouts.partials.footer')
     </div>
     <!-- ./wrapper -->
 
