2f39bc1d269bc1a708e980ca6aeec977d87fcb40
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-03 15:50:50 -0500

    Add: dinamic title
    titulo dinamico

--- app/Livewire/Home/Inicio.php
+++ app/Livewire/Home/Inicio.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Home;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Inicio extends Component
 {
+    #[Title('Inicio test')]
     public function render()
     {
         return view('livewire.home.inicio');
