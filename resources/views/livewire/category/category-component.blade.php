699896a00d44b2b4b59893e118955c1a30c4a19f
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-05 16:08:04 -0500

    Move: modals out

--- resources/views/livewire/category/category-component.blade.php
+++ resources/views/livewire/category/category-component.blade.php
@@ -51,21 +51,5 @@
             {{ $categories->links() }}
         </x-slot:cardFooter>
     </x-card>
-    <x-modal modalId="modalCategory" modalTitle="Categorias">
-        <form wire:submit={{ $Id == 0 ? 'store' : "update($Id)" }}>
-            <div class="form-row">
-                <div class="form-group col-12">
-                    <label for="name">Nombre: </label>
-                    <input wire:model="name" type="text" class="form-control" id="name"
-                        placeholder="Nombre de Categoria">
-                    @error('name')
-                        <div class="alert alert-danger w-100 mt-2">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-
-            <hr>
-            <button class="btn btn-primary float-right">{{ $Id == 0 ? 'Guardar' : 'Editar' }}</button>
-        </form>
-    </x-modal>
+    @include('categories.modal')
 </div>
---
+++ resources/views/products/modal.blade.php
@@ -0,0 +1,16 @@
+<x-modal modalId="modalProduct" modalTitle="Productos">
+    <form wire:submit={{ $Id == 0 ? 'store' : "update($Id)" }}>
+        <div class="form-row">
+            <div class="form-group col-12">
+                <label for="name">Nombre: </label>
+                <input wire:model="name" type="text" class="form-control" id="name" placeholder="Nombre de Producto">
+                @error('name')
+                    <div class="alert alert-danger w-100 mt-2">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+
+        <hr>
+        <button class="btn btn-primary float-right">{{ $Id == 0 ? 'Guardar' : 'Editar' }}</button>
+    </form>
+</x-modal>
