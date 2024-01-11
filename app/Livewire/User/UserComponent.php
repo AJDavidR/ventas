65fefa0c90eb98d23b767a5feddc88ad615db230
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-11 10:02:06 -0500

    Add: User basic schema
    añadir el esquema basico en componente y vista de usuario

--- app/Livewire/User/UserComponent.php
+++ app/Livewire/User/UserComponent.php
@@ -2,12 +2,37 @@
 
 namespace App\Livewire\User;
 
+use App\Models\User;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
+use Livewire\WithPagination;
 
+#[Title('Usuarios')]
 class UserComponent extends Component
 {
+    use WithFileUploads, WithPagination;
+
+    // ----------> propiedades clase
+    public $search = '';
+
+    public $totalRegistros = 0;
+
+    public $cant = 5;
+
+    // ----------> propiedades modelo
+    public $Id = 0;
     public function render()
     {
-        return view('livewire.user.user-component');
+        $this->totalRegistros = User::count();
+
+        $users = User::query()
+            ->where('name', 'like', "%{$this->search}%")
+            ->orderby('id', 'desc')
+            ->paginate($this->cant);
+
+        return view('livewire.user.user-component', [
+            'users' => $users,
+        ]);
     }
 }
--- resources/views/livewire/user/user-component.blade.php
+++ resources/views/livewire/user/user-component.blade.php
@@ -1,3 +1,77 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-card cardTitle="Listado categorias ({{ $this->totalRegistros }})">
+        <x-slot:cardTools>
+            <a class="btn btn-primary" wire:click='create'>
+                <i class="fas fa-plus-circle"></i> Crear
+            </a>
+        </x-slot>
+
+        <x-table>
+            <x-slot:thead>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th width="3%">Ver</th>
+                <th width="3%">Editar</th>
+                <th width="3%">Borrar</th>
+
+            </x-slot>
+
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>
+                        <a href="#" title="ver" class="btn btn-success btn-sm">
+                            <i class="fas fa-eye"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a wire:click='edit({{ $user->id }})' title="editar" class="btn btn-primary btn-sm">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a wire:click="$dispatch('delete',{
+                            id:{{ $user->id }},
+                            eventName: 'destroyProduct'
+                        })"
+                            title="eliminar" class="btn btn-danger btn-sm">
+                            <i class="fas fa-trash-alt"></i>
+                        </a>
+                    </td>
+                </tr>
+
+            @empty
+
+                <tr class="text-center">
+                    <td colspan="5">Sin registros</td>
+                </tr>
+            @endforelse
+
+        </x-table>
+
+        <x-slot:cardFooter>
+            {{ $users->links() }}
+
+        </x-slot>
+    </x-card>
+
+
+    <x-modal modalId="modalUser" modalTitle="Users">
+        <form wire:submit={{ $Id == 0 ? 'store' : "update($Id)" }}>
+            <div class="form-row">
+                <div class="form-group col-12">
+                    <label for="name">Nombre:</label>
+                    <input wire:model='name' type="text" class="form-control" placeholder="Nombre" id="name">
+                    @error('name')
+                        <div class="alert alert-danger w-100 mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
+            <hr>
+            <button class="btn btn-primary float-right">{{ $Id == 0 ? 'Guardar' : 'Editar' }}</button>
+        </form>
+    </x-modal>
+
 </div>
