529b782bad210fc931269ceefe9cab7b16c8b7f0
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-15 15:57:20 -0500

    Config: sale shcema
    convertir el sidebar en contraido por defecto
    mover la posicion del boton de ventas
    y añadir el esquema de ventas view

--- resources/views/livewire/sale/sale-create.blade.php
+++ resources/views/livewire/sale/sale-create.blade.php
@@ -1,10 +1,27 @@
 <div>
     <x-card cardTitle="Crear Venta">
         <x-slot:cardTools>
-            <a href="#" class="btn btn-primary" wire:click='create'>
-                <i class="fas fa-plus-circle"></i> Crear
+            <a href="#" class="btn btn-primary mr-2" wire:click='create'>
+                <i class="fas fa-arrow-circle-left mr-1"></i> Ir a ventas
             </a>
+
+            <a href="#" class="btn btn-danger" wire:click='create'>
+                <i class="fas fa-trash mr-1"></i> Cancelar venta
+            </a>
+
         </x-slot>
+
+        {{-- CONTENT --}}
+        <div class="row">
+            {{-- Detalles de la venta --}}
+            <div class="col-md-6">
+                Detalles de la venta
+            </div>
+            {{-- Columna productos --}}
+            <div class="col-md-6">
+                Columna productos
+            </div>
+        </div>
 
 
         <x-slot:cardFooter>
