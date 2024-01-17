33850b79e44c30bbe3e1437d0105f32704589889
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-01-17 16:40:23 -0500

    Add: store method client
    añadir el metodo de guardado de clientComponent en client

--- app/Livewire/Sale/Client.php
+++ app/Livewire/Sale/Client.php
@@ -1,4 +1,5 @@
 <?php
+// lo mejor es poner los metodos de clientComponent en un trait para reutilizarlos aqui y en clientComponent sin duplicar codigo
 
 namespace App\Livewire\Sale;
 
@@ -7,7 +8,21 @@ use Livewire\Component;
 
 class Client extends Component
 {
+    // ----------> propiedades modelo
+
     public $Id = 0;
+
+    public $name;
+
+    public $identificacion;
+
+    public $telefono;
+
+    public $email;
+
+    public $empresa;
+
+    public $nit;
     public $client;
 
     public function render()
@@ -23,5 +38,56 @@ class Client extends Component
         $this->Id = 0;
         // $this->resetInputFields();
         $this->dispatch('open-modal', 'modalClient');
+    }
+
+    // guardar el nuevo Cliente
+    public function store()
+    {
+        $rules = [
+            'name' => 'required|min:5|max:255',
+            // 'identificacion' => 'required|max:999999999999999|unique:clients|numeric',
+            // ^: Indica que la cadena debe comenzar aquí. // [0-9]: Representa cualquier dígito del 0 al 9.
+            // {6,10}: Indica que debe haber de 6 a 10 dígitos. // $: Indica que la cadena debe terminar aquí.
+            'identificacion' => 'required|regex:/^[0-9]{6,10}$/|numeric|unique:clients',
+            'telefono' => 'numeric|nullable',
+            'email' => 'nullable|Email|max:255',
+            'empresa' => 'nullable',
+            'nit' => 'numeric|nullable',
+        ];
+        $messages = [
+            'identificacion.regex' => 'El campo de identificación debe contener solo números y tener entre 6 y 10 dígitos.',
+        ];
+
+        $this->validate($rules, $messages);
+
+        $client = new Cliente();
+
+        $client->name = $this->name;
+        $client->identificacion = $this->identificacion;
+        $client->telefono = $this->telefono;
+        $client->email = $this->email;
+        $client->empresa = $this->empresa;
+        $client->nit = $this->nit;
+
+        $client->save();
+
+        $this->dispatch('close-modal', 'modalClient');
+        $this->dispatch('msg', 'Cliente creado correctamente');
+
+        $this->resetInputFields();
+    }
+
+    // resetear campos
+    public function resetInputFields()
+    {
+        $this->reset([
+            'name',
+            'identificacion',
+            'telefono',
+            'email',
+            'empresa',
+            'nit',
+        ]);
+        $this->resetErrorBag();
     }
 }
