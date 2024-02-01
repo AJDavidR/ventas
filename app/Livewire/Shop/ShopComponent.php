22af48e98489b32c91b07f56f743cd194cc84e88
AJDavidR <151045841+AJDavidR@users.noreply.github.com>
2024-02-01 15:56:50 -0500

    Config: Shop Crud
    crear todos los metodos de Shop

--- app/Livewire/Shop/ShopComponent.php
+++ app/Livewire/Shop/ShopComponent.php
@@ -2,12 +2,191 @@
 
 namespace App\Livewire\Shop;
 
+use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
+use Livewire\WithPagination;
 
+#[Title('Tienda')]
 class ShopComponent extends Component
 {
+    use WithFileUploads, WithPagination;
+
+    // ----------> propiedades clase
+    public $search = '';
+
+    public $totalRegistros = 0;
+
+    public $cant = 5;
+
+    // ----------> propiedades modelo
+    public $Id = 0;
+
+    public $name;
+
+    public $slogan;
+
+    public $telefono;
+
+    public $email;
+
+    public $direccion;
+
+    public $ciudad;
+
+    public $image;
+
+    public $imageModel;
+
     public function render()
     {
-        return view('livewire.shop.shop-component');
+        $this->totalRegistros = Shop::count();
+
+        $shops = Shop::query()
+            ->where('name', 'like', "%{$this->search}%")
+            ->orderby('id', 'desc')
+            // ->sortBy('id', 'desc')
+            ->paginate($this->cant);
+
+        return view('livewire.shop.shop-component', [
+            'shops' => $shops,
+        ]);
+    }
+
+    // resetear campos
+    public function resetInputFields()
+    {
+        $this->reset([
+            'name',
+            'slogan',
+            'telefono',
+            'email',
+            'direccion',
+            'ciudad',
+            'image',
+            'imageModel',
+        ]);
+        $this->resetErrorBag();
+    }
+
+    // Crear la tienda
+    public function create()
+    {
+        $this->Id = 0;
+        $this->resetInputFields();
+        $this->dispatch('open-modal', 'modalShop');
+    }
+
+    // guardar el nueva tienda
+    public function store()
+    {
+        // dump('Crear shop');
+        $rules = [
+            'name' => 'required|min:5|max:255|unique:shops',
+            'slogan' => 'max:255|nullable',
+            'telefono' => 'numeric|nullable',
+            'email' => 'max:255|nullable|Email',
+            'direccion' => 'max:255|nullable',
+            'ciudad' => 'max:255|nullable',
+            'image' => 'image|max:1024|nullable',
+        ];
+        $this->validate($rules);
+
+        $shop = new Shop();
+
+        $shop->name = $this->name;
+        $shop->slogan = $this->slogan;
+        $shop->telefono = $this->telefono;
+        $shop->email = $this->email;
+        $shop->direccion = $this->direccion;
+        $shop->ciudad = $this->ciudad;
+
+        $shop->save();
+
+        if ($this->image) {
+            $newName = 'shop/' . uniqid() . '.' . $this->image->extension();
+            $this->image->storeAs('public', $newName);
+            $shop->image()->create(['url' => $newName]);
+        }
+
+        $this->dispatch('close-modal', 'modalShop');
+        $this->dispatch('msg', 'tienda creado correctamente');
+
+        $this->resetInputFields();
+    }
+
+    // Editar tienda
+    public function edit(Shop $shop)
+    {
+        $this->resetInputFields();
+
+        $this->Id = $shop->id;
+        $this->name = $shop->name;
+        $this->slogan = $shop->slogan;
+        $this->telefono = $shop->telefono;
+        $this->email = $shop->email;
+        $this->direccion = $shop->direccion;
+        $this->ciudad = $shop->ciudad;
+
+        $this->dispatch('open-modal', 'modalShop');
+    }
+
+    // Actualizar el tienda
+    public function update(Shop $shop)
+    {
+        // dump($shop);
+        $rules = [
+            'name' => 'required|min:5|max:255|unique:shops,'.$this->Id,
+            'slogan' => 'max:255|nullable',
+            'telefono' => 'numeric|nullable',
+            'email' => 'max:255|nullable|Email',
+            'direccion' => 'max:255|nullable',
+            'ciudad' => 'max:255|nullable',
+            'image' => 'image|max:1024|nullable',
+        ];
+        $this->validate($rules);
+
+        $shop->name = $this->name;
+        $shop->slogan = $this->slogan;
+        $shop->telefono = $this->telefono;
+        $shop->email = $this->email;
+        $shop->direccion = $this->direccion;
+        $shop->ciudad = $this->ciudad;
+
+        $shop->update();
+
+        if ($this->image) {
+            if ($shop->image != null) {
+                Storage::delete('public/' . $shop->image->url);
+                $shop->image()->delete();
+            }
+            $newName = 'shops/' . uniqid() . '.' . $this->image->extension();
+            $this->image->storeAs('public', $newName);
+            $shop->image()->create(['url' => $newName]);
+        }
+
+        $this->dispatch('close-modal', 'modalShop');
+        $this->dispatch('msg', 'Tienda editado correctamente');
+
+        $this->resetInputFields();
+    }
+
+    // eliminar tienda
+    #[On('destroyShop')]
+    public function destroy($id)
+    {
+        // dump($id);
+        $shop = Shop::findOrFail($id);
+        $shop->delete();
+
+        if ($shop->image != null) {
+            Storage::delete('public/' . $shop->image->url);
+            $shop->image()->delete();
+        }
+
+        $this->dispatch('msg', 'Tienda eliminado correctamente');
     }
 }
--- resources/views/livewire/shop/shop-component.blade.php
+++ resources/views/livewire/shop/shop-component.blade.php
@@ -1,156 +1,76 @@
 <div>
     <x-card cardTitle="Datos tienda">
-       <x-slot:cardTools>
-          <a href="#" class="btn btn-primary" wire:click='edit'>
-            <i class="fas fa-edit"></i> Editar
-          </a>
-       </x-slot>
+        <x-slot:cardTools>
+            <a class="btn btn-primary" wire:click='create'>
+                <i class="fas fa-plus-circle"></i> Crear
+            </a>
+        </x-slot>
 
-       <x-table>
-          <x-slot:thead>
-             <th>ID</th>
-             <th>
-                <i class="fas fa-image"></i>
-            </th>
-             <th>Nombre</th>
-             <th>Slogan</th>
-             <th>Telefono</th>
-             <th>Email</th>
-             <th>Direccion</th>
-             <th>Ciudad</th>
- 
-          </x-slot>
+        <div class="table-responsive">
 
-          @forelse ($shops as $shop)
-              
-             <tr>
-                <td>{{$shop->id}}</td>
-                <td>
-                    <x-image :item="$shop" />
-                </td>
-                <td>{{$shop->name}}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            <table class="table table-hover table-striped text-center">
+                <thead>
+                    <th>ID</th>
+                    <th>
+                        <i class="fas fa-image"></i>
+                    </th>
+                    <th>Nombre</th>
+                    <th>Slogan</th>
+                    <th>Telefono</th>
+                    <th>Email</th>
+                    <th>Direccion</th>
+                    <th>Ciudad</th>
+                    <th width="3%">Editar</th>
+                    <th width="3%">Borrar</th>
 
-             </tr>
+                </thead>
 
-             @empty
+                <tbody>
+                    @forelse ($shops as $shop)
+                        <tr>
+                            <td>{{ $shop->id }}</td>
+                            <td>
+                                <x-image :item="$shop" />
+                            </td>
+                            <td>{{ $shop->name }}</td>
+                            <td>{{ $shop->slogan }}</td>
+                            <td>{{ $shop->telefono }}</td>
+                            <td>{{ $shop->email }}</td>
+                            <td>{{ $shop->direccion }}</td>
+                            <td>{{ $shop->ciudad }}</td>
+                            <td>
+                                <button wire:click='edit({{ $shop->id }})' class="btn btn-primary float-right">
+                                    Editar
+                                </button>
+                            </td>
+                            <td>
+                                <a wire:click="$dispatch('delete',{
+                                    id:{{ $shop->id }},
+                                    eventName: 'destroyShop'
+                                })"
+                                    title="eliminar" class="btn btn-danger btn-sm">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </td>
 
-             <tr class="text-center">
-                <td colspan="10">Sin registros</td>
-             </tr>
-              
-             @endforelse
- 
-       </x-table>
- 
-       <x-slot:cardFooter>   
-       </x-slot>
-    </x-card>
+                        </tr>
 
+                    @empty
 
-{{-- MODAL EDITAR --}}
-<x-modal modalId="modalShop" modalTitle="Datos tienda" modalSize="modal-lg">
-    <form wire:submit="edit">
-
-        <div class="form-row">
-
-            {{-- Input Name --}}
-            <div class="form-group col-md-5">
-                <label for="name">Nombre:</label>
-                <input wire:model='name' type="text" class="form-control" placeholder="Nombre tienda" id="name">
-                @error('name')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-            {{-- Input Slogan --}}
-            <div class="form-group col-md-7">
-                <label for="slogan">Slogan:</label>
-                <input wire:model='slogan' type="text" class="form-control" placeholder="Slogan tienda" id="slogan">
-                @error('slogan')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-
-            {{-- Input Telefono --}}
-            <div class="form-group col-md-5">
-                <label for="telefono">Telefono:</label>
-                <input wire:model='telefono' type="text" class="form-control" placeholder="Telefono tienda" id="telefono">
-                @error('telefono')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-
-            {{-- Input Telefono --}}
-            <div class="form-group col-md-7">
-                <label for="email">Email:</label>
-                <input wire:model='email' type="email" class="form-control" placeholder="Email tienda" id="email">
-                @error('email')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-            {{-- Input Direccion --}}
-            <div class="form-group col-md-5">
-                <label for="direccion">Direccion:</label>
-                <input wire:model='direccion' type="text" class="form-control" placeholder="Direccion tienda" id="direccion">
-                @error('direccion')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-            {{-- Input Ciudad --}}
-            <div class="form-group col-md-7">
-                <label for="ciudad">Ciudad:</label>
-                <input wire:model='ciudad' type="text" class="form-control" placeholder="Ciudad tienda" id="ciudad">
-                @error('ciudad')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-
-            {{-- Input imagen --}}
-            <div class="form-group col-md-6">
-
-                <label for="image">Imagen:</label>
-                <input wire:model='image' type="file" id="image" accept="image/*">
-                
-                @error('image')
-                    <div class="alert alert-danger w-100 mt-2">{{$message}}</div>
-                @enderror
-            </div>
-            
-            {{-- imagen --}}
-            <div class="form-group col-md-6">
-
-            
-                <x-image :item="$product= App\Models\Shop::find(1)" size="200" float="float-right" />
-    
-
-                @if ($this->image)
-                <img src="{{$image->temporaryUrl()}}" class="rounded float-right" width="200">
-                @endif
-
-            </div>
-
+                        <tr class="text-center">
+                            <td colspan="10">Sin registros</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
 
         </div>
-        
-        <hr>
-    <button wire:loading.attr='disabled' class="btn btn-primary float-right">
-        Editar
-    </button>    
 
-    </form>
- </x-modal>
+        <x-slot:cardFooter>
+            {{ $shops->links() }}
+        </x-slot>
+    </x-card>
 
-
-
-
+    @include("shops.modal")
 
 </div>
